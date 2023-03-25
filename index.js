fa2dd600f4d1d535b1476ba443a267d9baec1c1f
trevor wells <104236134+trevor-wells@users.noreply.github.com>
2023-03-25 11:47:48 -0600

    final commit

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Code your solution here
+function findMatching(names, str){
+    return names.filter(element => element.toLowerCase() === str.toLowerCase())
+}
+
+function fuzzyMatch(names, str){
+    return names.filter(element => element.startsWith(str))
+}
+
+function matchName(names, str){
+    return names.filter(element => element.name === str)
+}
